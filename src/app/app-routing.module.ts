7a41c944fbc855ef719d072eac8ea9a8f3928039
Victor Augusto de Mattos Carbelotti <victor.carbelotti@etec.sp.gov.br>
2022-12-08 21:16:11 -0300

    Criação app de Tempo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'tempo',
+    loadChildren: () => import('./modals/tempo/tempo.module').then( m => m.TempoPageModule)
+  }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
